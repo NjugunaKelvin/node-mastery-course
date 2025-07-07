3bb6758219649b5e70fa92917a9d665cf1dae9c1
NjugunaKelvin <njugunak349@gmail.com>
2025-07-07 13:31:07 +0300

    working with modules, exports and require

---
+++ module1.js
@@ -0,0 +1,7 @@
+const names = ['vin', 'jack', 'Mary', 'vivian'];
+const ages = [21,23,24,24];
+
+module.exports = {
+    ages,names
+}
+
---
+++ module2.js
@@ -0,0 +1,3 @@
+const {ages, names} = require('./module1');
+console.log(ages);
+console.log(names);

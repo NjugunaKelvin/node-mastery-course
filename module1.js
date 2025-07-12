3d43422549aeeaa620fbb8a4939e9620611a4eb5
NjugunaKelvin <njugunak349@gmail.com>
2025-07-12 14:15:18 +0300

    removed unnecessary files

--- module1.js
+++
@@ -1,7 +0,0 @@
-const names = ['vin', 'jack', 'Mary', 'vivian'];
-const ages = [21,23,24,24];
-
-module.exports = {
-    ages,names
-}
-
--- module2.js
+++
@@ -1,3 +0,0 @@
-const {ages, names} = require('./module1');
-console.log(ages);
-console.log(names);

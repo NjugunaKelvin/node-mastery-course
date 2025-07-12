e73bda803a90f010f69b4d7885f7d28f476af8fc
NjugunaKelvin <njugunak349@gmail.com>
2025-07-12 14:07:27 +0300

    servers and basic node routing

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
+// require the http and the fs module
+const fs = require('fs');
 const http = require('http');
-const server = http.createServer((req, res) => {
-   console.log('Request made.'); 
+
+// create a server 
+const myServer = http.createServer((req,res) => {
+    console.log('A response was made');
+
+    // set headers;
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'home.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            res.statusCode = 404;
+            break;
+                   
+    }
+
+    // read the html files
+    fs.readFile(path, (err,data) =>{
+        if (err) {
+            console.log('An error occurred reading the html file');
+        }
+        else {
+            res.end(data)
+        }
+    });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
-})
+// listen
+myServer.listen(3000, 'localhost', () =>{
+    console.log('server running well');
+});
